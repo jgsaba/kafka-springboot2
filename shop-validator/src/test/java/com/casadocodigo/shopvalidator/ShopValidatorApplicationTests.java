073984262a2181f6d050d7da69c4104624d8f819
pve_jjorge <pve_jjorge@uolinc.com>
2022-03-15 10:52:57 -0300

    .

---
+++ shop-validator/src/test/java/com/casadocodigo/shopvalidator/ShopValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.shopvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
