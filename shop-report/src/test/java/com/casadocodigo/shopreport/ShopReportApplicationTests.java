c78fd617c19f83857cb3bcd810c5ee60040ee29f
pve_jjorge <pve_jjorge@uolinc.com>
2022-03-16 20:01:21 -0300

    ..

---
+++ shop-report/src/test/java/com/casadocodigo/shopreport/ShopReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casadocodigo.shopreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
